d4471b549ebb64e4ab7d5a04dd3d8035e5638ad5
Nipuna Shanthidewa <nipunashanthidewa16@gmail.com>
2021-09-19 16:15:40 +1000

    add database models for image

--- images/models.py
+++ images/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+import uuid
+
+
+def user_directory_path(instance, filename):
+    # file will be uploaded to MEDIA_ROOT / user_<id>/<filename>
+    ext = filename.split('.')[-1]
+    filename = "%s.%s" % (uuid.uuid4(), ext)
+    return 'data/user_{0}/{1}'.format(instance.uploader.id, filename)
+
+
+class Image(models.Model):
+    name = models.CharField(max_length=100)
+    upload = models.ImageField(upload_to=user_directory_path)
+    uploader = models.ForeignKey(User, on_delete=models.CASCADE, related_name="images")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
